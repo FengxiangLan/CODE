30026de6de1b3ad606f38d49f69c04f23324e4a2
FengxiangLan <31476093+FengxiangLan@users.noreply.github.com>
2019-01-12 21:50:48 -0500

    Add files via upload

--- Tree/LevelTraversal.java
+++ Tree/LevelTraversal.java
@@ -1,3 +1,28 @@
 public class LevelTraversal {
-	public List<List<Integer>> layerByLayer(TreeNode root)
+	public List<List<Integer>> layerByLayer(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if (root == null) {
+			return res;
+		}
+
+		Queue<TreeNode> nodeList = new LinkedList<TreeNode>();
+		nodeList.offer(root);
+		while (!nodeList.isEmpty()) {
+			List<Integer> curLayer = new ArrayList<Integer>();
+			int size = nodeList.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode cur = nodeList.poll();
+				curLayer.add(cur.key);
+				if (cur.left != null) {
+					nodeList.offer(cur.left);
+				}
+
+				if (cur.right != null) {
+					nodeList.offer(cur.right);
+				}
+			}
+			res.add(curLayer);
+		}
+		return res;
+	}
 }
