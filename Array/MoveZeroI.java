aa3b018a04659664f7aa526942cf07065cacabfb
FengxiangLan <31476093+FengxiangLan@users.noreply.github.com>
2019-02-01 19:56:07 -0500

    Add files via upload

---
+++ Array/MoveZeroI.java
@@ -0,0 +1,27 @@
+public class MoveZeroI {
+	public int[] moveZero(int[] array) {
+		if (array == null || array.length == 0) {
+			return array;
+		}
+		int left = 0;
+		int right = array.length - 1;
+		while (left <= right) {
+			if (array[left] != 0) {
+				left++;
+			}
+			else if (array[right] == 0) {
+				right--;
+			}
+			else {
+				swap(array, left++, right--);
+			}
+		}
+		return array;
+	}
+
+	private void swap(int[] array, int left, int right){
+		int temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
+	}
+}
