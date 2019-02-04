a6c00c6ab1c98e230412b62cfb05a23695fa1f6b
FengxiangLan <31476093+FengxiangLan@users.noreply.github.com>
2019-02-03 21:05:34 -0500

    Add files via upload

---
+++ Heap/HeapOfferOperation.java
@@ -0,0 +1,29 @@
+public class HeapOfferOperation {
+	public int[] offerHeap(int[] array, int ele) {
+		array[array.length - 1] = ele;
+		if (array.length == 1) {
+			return array;
+		}
+
+		percolateUp(array, array.length - 1);
+		return array;
+	}
+
+	private void percolateUp(int[] array, int index) {
+		while (index > 0) {
+			int parentIndex = (index - 1) / 2;
+			if (array[index] < array[parentIndex]) {
+				swap(array, index, parentIndex);
+			} else {
+				break;
+			}
+			index = parentIndex;
+		}
+	}
+
+	private void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
