6adb5dddd2c4d3b2ea402d730ab113027bf8cd2d
FengxiangLan <31476093+FengxiangLan@users.noreply.github.com>
2019-02-09 00:14:11 -0500

    Add files via upload

--- Array/TwoSumTwoArray.java
+++ Array/TwoSumTwoArray.java
@@ -1,1 +1,16 @@
-public class TwoSumTwoArray.java
+public class TwoSumTwoArray {
+	public boolean existSum(int[] a, int[] b, int target) {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < a.length; i++) {
+			set.add(a[i]);
+		}
+
+		for (int i = 0; i < b.length; i++) {
+			if (set.contains(target - b[i])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
