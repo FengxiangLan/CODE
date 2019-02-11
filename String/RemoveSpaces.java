c068c70482a0e58aa5bb8476cd0c28d61f33827c
FengxiangLan <31476093+FengxiangLan@users.noreply.github.com>
2019-02-11 15:00:00 -0500

    Add files via upload

---
+++ String/RemoveSpaces.java
@@ -0,0 +1,30 @@
+public class RemoveSpaces {
+	public String removeSpaces(String input) {
+		// Given a string, remove all leading/trailing/duplicated empty spaces.
+		// if fast == " " and (fast == start or fast - 1 == " ")
+		//  fast++;
+		// else 
+		// slow ++ = fast ++
+		if (input == null || input.length() == 0) {
+			return input;
+		}
+
+		char[] inputArray = input.toCharArray();
+		int slow = 0;
+		int fast = 0;
+		while (fast < input.length) {
+			if (inputArray[fast] == ' ' && (fast == 0) || inputArray[fast - 1] == ' ') {
+				fast++;
+			} else {
+				inputArray[slow++] = inputArray[fast++];
+			}
+		}
+
+		if (slow > 0 && inputArray[slow - 1] == ' ') {
+			return new String(inputArray, 0, slow - 1);
+		}
+
+		return new String(inputArray, 0, slow);
+
+	}
+}
